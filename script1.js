8311e0a276e8452cc00545740e18dfdb9c4fa06d
alishba200 <153546780+alishba200@users.noreply.github.com>
2024-08-15 20:23:30 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+//Q1
+var var1=2, var2=4, var3=6;
+//Q2
+var myVariable;
+var $dollarSign;
+var _underscore;
+var camelCaseVar;
+var variable123;
+
+// var 1variable;        // Starts with a number
+// var my-variable;      // Contains hyphen (-)
+// var var;              // Reserved keyword
+// var my variable;      // Contains space
+// var @specialChar;     // Starts with special character other than $ or _
